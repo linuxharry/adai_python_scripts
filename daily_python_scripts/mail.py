199de2ab62b0ee174e50c0df2dc01ab244ce05da
adai <adai_mail@126.com>
2019-09-16 10:12:47 +0800

    fix

--- daily_python_scripts/mail.py
+++ daily_python_scripts/mail.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-#coding:utf-8
+# -*- coding: utf-8 -*-
+
+# **注意：** python2.6/2.7版本没问题，python3有问题
+
 import smtplib
 from email.mime.text import MIMEText
 import sys
@@ -7,6 +10,8 @@ mail_host = 'smtp.163.com'
 mail_user = 'test@163.com'
 mail_pass = 'your_mail_password'
 mail_postfix = '163.com'
+
+
 def send_mail(to_list,subject,content):
     me = "zabbix 监控告警平台"+"<"+mail_user+"@"+mail_postfix+">"
     msg = MIMEText(content, 'plain', 'utf-8')
@@ -23,9 +28,10 @@ def send_mail(to_list,subject,content):
     except Exception,e:
         print str(e)
         return False
+
+
 if __name__ == "__main__":
     send_mail(sys.argv[1], sys.argv[2], sys.argv[3])
 
-## **注意：** python2.6/2.7版本没问题，python3有问题
 
 
